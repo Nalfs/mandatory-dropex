a61541d2b634c339751dda43a2b240ac5ac75513
Khoi Le <minarek@outlook.com>
2018-05-24 23:45:40 +0200

    Improved createObjFromParams() and added trustUrl into dropboxConfig obj

--- src/app/configs.ts
+++ src/app/configs.ts
@@ -2,7 +2,8 @@
 export const dropboxConfig = {
     clientId: 'tefcr0lybxy6atz',
     redirectUri: 'http://localhost:4200/auth',
-    responseType: 'token'
+    responseType: 'token',
+    trustUrl: 'https://www.dropbox.com'
 };
 
 export const firestoreConfig = {
--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,32 +1,18 @@
-export function getParamFromUrl(param) {
-    try {
-        const sQueryString = document.URL.split('#')[1] || document.URL.split('?')[1] || '';
-        const searchParams = new URLSearchParams(sQueryString);
-        if (searchParams.has(param)) {
-            return searchParams.get(param);
-        } else {
-            return '';
-        }
-    } catch (error) {
-        return error;
-    }
-}
+import { dropboxConfig } from './configs';
 
 export function createObjFromParams() {
     try {
         const sQueryString = document.URL.split('#')[1] || document.URL.split('?')[1] || '';
         const referrerUrl = document.referrer;
-        const trustUrl = 'https://www.dropbox.com';
 
         // This object's properties must be like the return URL from Dropbox API
-        const objParams = {
-            access_token: '',
-            token_type: '',
-            uid: '',
-            account_id: ''
-        };
+        const objParams = { access_token: '',
+                            token_type: '',
+                            uid: '',
+                            account_id: ''
+                        };
 
-        if (sQueryString && referrerUrl.substring(0, trustUrl.length) === trustUrl) {
+        if (sQueryString && referrerUrl.startsWith(dropboxConfig.trustUrl)) {
             const arrParams = sQueryString.split('&');
             for (let i = 0; i < arrParams.length; i++) {
                 const item = arrParams[i].split('=');
