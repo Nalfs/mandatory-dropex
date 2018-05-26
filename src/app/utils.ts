a54ebf93fa8916b3df01dce9057b5878b5d41284
Khoi Le <minarek@outlook.com>
2018-05-27 00:04:59 +0200

    Added function getParamFromUrl()

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,5 +1,19 @@
 import { dropboxConfig } from './configs';
 
+export function getParamFromUrl(param) {
+    try {
+        const sQueryString = document.URL.split('#')[1] || document.URL.split('?')[1] || '';
+        const searchParams = new URLSearchParams(sQueryString);
+        if (searchParams.has(param)) {
+            return searchParams.get(param);
+        } else {
+            return '';
+        }
+    } catch (error) {
+        return error;
+    }
+}
+
 export function createObjFromParams() {
     try {
         const sQueryString = document.URL.split('#')[1] || document.URL.split('?')[1] || '';
