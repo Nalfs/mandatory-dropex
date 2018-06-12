9485808e97e026b1f0ca67002278f69c6fc201e2
Khoi Le <minarek@outlook.com>
2018-06-12 23:42:04 +0200

    Fixed bug for breadcrumbs

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -1,59 +1,58 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, Routes } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { FilesService } from './../files.service';
+// import { FilesService } from './../files.service'; // Deleted by K - Declared but don't use
 
 @Component({
-  selector: 'app-breadcrumbs',
-  templateUrl: './breadcrumbs.component.html',
-  styleUrls: ['./breadcrumbs.component.css']
+    selector: 'app-breadcrumbs',
+    templateUrl: './breadcrumbs.component.html',
+    styleUrls: ['./breadcrumbs.component.css']
 })
 export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  private subscription: Subscription;
-    pathArray;
-    goBack;
-    parentFolder;
-    currentPath;
+    private subscription: Subscription;
+    public pathArray;
+    // private goBack; // Deleted by K
+    public parentFolder;
+    // private currentPath; // Deleted by K
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private router: Router,
-              private filesService: FilesService) {
-  }
+    constructor(private activatedRoute: ActivatedRoute,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-      this.subscription = this.activatedRoute.url.subscribe(() => {
-        const urlWithoutParams = decodeURIComponent(this.router.url).split('?')[0]; // Added by K
-        this.pathArray = this.getPathsToRenderFromUrl(urlWithoutParams);
-        this.goBack = this.goBackFn(this.currentPath);
-      });
-  }
-
-  getPathsToRenderFromUrl(currentPath) {
-    let paths = currentPath.split('/');
-    if (currentPath === '' || currentPath === '/' ) {
-    paths = [''];
-  }
-    let fullpath = '';
-    const pathsToRender = [];
-    for (let i = 0; i < paths.length; i++) {
-        const path = decodeURI(paths[i]);
-        fullpath += `/${path}`;
-        pathsToRender.push({
-          path,
-          fullpath,
+    ngOnInit() {
+        this.subscription = this.activatedRoute.url.subscribe(() => {
+            const urlWithoutParams = decodeURIComponent(this.router.url).split('?')[0]; // Added by K
+            this.pathArray = this.getPathsToRenderFromUrl(urlWithoutParams); // Edited by K
+            // this.goBack = this.goBackFn(); // Deleted by K
+            this.goBackFn();
         });
-      }
-    return pathsToRender;
-  }
+    }
 
-  goBackFn(currentPath) {
-    return this.parentFolder = this.pathArray.slice(0, -1);
-}
+    getPathsToRenderFromUrl(currentPath) {
+        let paths = currentPath.split('/');
+        if (currentPath === '' || currentPath === '/') {
+            paths = [''];
+        }
+        let fullpath = '';
+        const pathsToRender = [];
+        for (let i = 0; i < paths.length; i++) {
+            const path = decodeURI(paths[i]);
+            fullpath += `/${path}`;
+            pathsToRender.push({
+                path,
+                fullpath,
+            });
+        }
+        return pathsToRender;
+    }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
-}
+    goBackFn() {
+        return this.parentFolder = this.pathArray.slice(0, -1);
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
 }
